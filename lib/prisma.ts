5e1ed978cc0efd3e4124722690d255be5e6de41a
kellan-dev <174945686+kellan-dev@users.noreply.github.com>
2024-11-20 07:52:38 -0500

    Database setup

---
+++ lib/prisma.ts
@@ -0,0 +1,43 @@
+// Combines db.ts recommendations from both of these pages:
+// https://www.prisma.io/docs/orm/overview/databases/neon
+// https://www.prisma.io/docs/orm/more/help-and-troubleshooting/help-articles/nextjs-prisma-client-dev-practices
+
+import { Pool, neonConfig } from "@neondatabase/serverless";
+import { PrismaNeon } from "@prisma/adapter-neon";
+import { PrismaClient } from "@prisma/client";
+import ws from "ws";
+
+neonConfig.webSocketConstructor = ws;
+const connectionString = process.env.DATABASE_URL;
+const directConnectionString = process.env.DIRECT_URL;
+
+if (!connectionString) {
+  throw new Error("DATABASE_URL is not set");
+}
+
+if (!directConnectionString) {
+  throw new Error("DIRECT_URL is not set");
+}
+
+const prismaClientSingleton = () => {
+  const pool = new Pool({ connectionString });
+  const adapter = new PrismaNeon(pool);
+  return new PrismaClient({
+    adapter,
+    datasources: {
+      db: {
+        url: directConnectionString,
+      },
+    },
+  });
+};
+
+declare global {
+  var prisma: ReturnType<typeof prismaClientSingleton> | undefined;
+}
+
+const prisma = globalThis.prisma ?? prismaClientSingleton();
+
+export default prisma;
+
+if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
